9a170db00d60de96d57e123708334671c89c6bee
Luiz Guilherme Bestel <luizgbes@gmail.com>
2022-05-05 16:08:36 -0300

    requisitos do 1 ao 3

---
+++ script.js
@@ -0,0 +1,11 @@
+function login () {
+  const email = document.querySelector('#email').value;
+  const senha = document.querySelector('#senha').value;
+
+  if (email === 'tryber@teste.com' && senha === '123456') {
+    window.alert('Olá, Tryber!');
+  } else {
+    window.alert('Email ou senha inválidos.');
+  }
+}
+document.querySelector('#entrar').addEventListener('click', login);
