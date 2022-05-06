68c85d1781e673edb2cdc815eeff71f3b057d900
leonardo-schneider2 <leocoelho256@gmail.com>
2022-05-06 12:25:20 -0300

    exercicios 1 ao 21 finalizados

--- script.js
+++ script.js
@@ -30,3 +30,61 @@ function count() {
 }
 
 document.querySelector('#textarea').addEventListener('input', count);
+
+const btn = document.querySelector('#submit-btn');
+
+function section1() {
+  const nome = document.querySelector('#input-name').value;
+  const sobrenome = document.querySelector('#input-lastname').value;
+  const email = document.querySelector('#input-email').value;
+  const house = document.querySelector('#house').value;
+
+  const section4 = document.querySelector('#section4');
+  section4.innerHTML = `Nome: ${nome} ${sobrenome}<br>Email: ${email}<br> Casa: ${house}`;
+}
+
+function section2() {
+  let family;
+  const familyArray = document.querySelectorAll('#section2-family input');
+  for (let i = 0; i < familyArray.length; i += 1) {
+    if (familyArray[i].checked === true) {
+      family = familyArray[i].value;
+    }
+  }
+  const tec = document.querySelectorAll('#section2-content input');
+  const tecArray = [];
+  for (let i = 0; i < tec.length; i += 1) {
+    if (tec[i].checked === true) {
+      tecArray.push(` ${tec[i].value}`);
+    }
+  }
+  const section4 = document.querySelector('#section4');
+  section4.innerHTML += `<br>Família: ${family}<br>Matérias: ${tecArray}`;
+}
+
+function section3() {
+  const rate = document.querySelectorAll('#avaliacao-container input');
+  let nota;
+  for (let i = 0; i < rate.length; i += 1) {
+    if (rate[i].checked === true) {
+      nota = rate[i].value;
+    }
+  }
+  console.log(nota);
+  const coment = document.querySelector('textarea').value;
+  console.log(coment);
+
+  const section4 = document.querySelector('#section4');
+  section4.innerHTML += `<br>Avaliação: ${nota}<br>Observações: ${coment}`;
+}
+
+btn.addEventListener('click', function(element) {
+  element.preventDefault();
+  const section4 = document.createElement('section');
+  section4.id = 'section4';
+  document.querySelector('#evaluation-form').appendChild(section4);
+
+  section1();
+  section2();
+  section3();
+});
